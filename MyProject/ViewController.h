ae133d7ab92ec64d764f3fc1a6d532151ae73f1c
Sathish08tech <Amogh@dsatishs-macbook-pro.local>
2016-02-28 18:48:17 +0530

    Initial commit

--- MyProject/ViewController.h
+++ MyProject/ViewController.h
@@ -10,6 +10,7 @@
 
 @interface ViewController : UIViewController
 
+- (IBAction)btnPressed:(id)sender;
 
 @end
 
--- MyProject/ViewController.m
+++ MyProject/ViewController.m
@@ -24,4 +24,8 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)btnPressed:(id)sender {
+    
+    NSLog(@"Button is pressed");
+}
 @end
